85a50d5e2d6cf3e107fdb10066e2008a119c54e7
nstonic <118763806+nstonic@users.noreply.github.com>
2023-01-14 14:30:38 +0400

    Get count of active cards

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
+cards = Passcard.objects.all()
+some_card = cards[0]
+
 if __name__ == '__main__':
     # Программируем здесь
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print('Активных пропусков:', Passcard.objects.filter(is_active=True).count())  # noqa: T001
+
